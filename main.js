1fac9c8f08ef3fe0c6aa7f2711e3a8e7d745c752
osj <ojsanch@gmail.com>
2020-12-08 13:00:15 -0600

    mobile ui completed

---
+++ main.js
@@ -0,0 +1,31 @@
+// Navbar toggle
+function navbarToggle() {
+	var menu = document.querySelector('.mylinks');
+	if (menu.style.display === 'block') {
+		menu.style.display = 'none';
+	} else {
+		menu.style.display = 'block';
+	}
+}
+
+// Gitub Repos Function
+async function getGithubRepos() {
+	const ulForGithubRepos = document.querySelector('.githubRepos');
+	const myGithubName = 'heyosj';
+
+	let response = await fetch(`https://api.github.com/users/${myGithubName}/repos`);
+	let data = await response.json();
+	for (let i = 1; i < data.length; i++) {
+		ulForGithubRepos.innerHTML += `
+            <li>
+                <a href="${data[i].html_url}" target="_blank">${data[i].name}</a>
+            </li>
+        `;
+	}
+
+	if (!response.ok) {
+		throw Error(response.statusText);
+	}
+}
+
+getGithubRepos();
